e41012710238b58999c49a06b889ec11fa07c9f6
Aaron Begy <abegy@miletwo.us>
2022-11-07 13:08:42 -0500

    updating code with input box

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,56 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react'
 import fetch from 'node-fetch'
+import React from 'react'
+import { useForm } from 'react-hook-form'
 import useSWR from 'swr'
+import { createSemicolonClassElement } from 'typescript'
+
 
 
 export default function Home() {
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+  const [label, setlabel] = useState("")
+
+  function handleSubmit(event:React.FormEvent<HTMLFormElement>) {
+    alert('A name was submitted: ' + label);
+    event.preventDefault();
+    const str1 = 'https://alt.edge.mile-two.com/api/tag/'
+    const str2 = str1.concat('', label);
+    // fetch(str2);
+    useEffect(() => {
+      setLoading(true)
+      fetch(str2)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data)
+          setData(data)
+          setLoading(false)
+        })
+    }, [])
+
+  }
+
+  function handleChange(event:React.ChangeEvent<HTMLInputElement>) {
+    setlabel(event.target.value)
+  }
+
+  // useEffect(() => {
+  //   setLoading(true)
+  //   fetch('/api/test')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data)
+  //       setData(data)
+  //       setLoading(false)
+  //     })
+  // }, [])
+  
+  if (isLoading) return <p>Loading...</p>
+  // if (!data) return <p>No profile data</p>
+
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -19,10 +65,16 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to my Example App
         </h1>
-
         <p className={styles.description}>
           Manually search the label intel in the <a href="http://localhost:3000/api/test">AltRestAPI</a>
         </p>
+        <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={label} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
         <div>
     </div>
       </main>
